0f070e17b3ef5e9186e813a758c8e9f10a354cec
Oleh Moroz <oleh.moroz01@gmail.com>
2023-05-03 21:20:22 +0300

    Test commit -  10

--- src/test/java/LessonTenth.java
+++ src/test/java/LessonTenth.java
@@ -1,38 +1,32 @@
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import java.util.Map;
 import java.util.HashMap;
 
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.given;
 
 public class LessonTenth {
-
-    private static final String BASE_URL = "https://api.novaposhta.ua/v2.0/json/";
-    private static final String API_KEY = "def7e7bd2304d0aa8c0a788f596985b6";
-
     @Test
-    public void testCitiesSearch() {
-        Map<String, Object> methodProperties = new HashMap<>();
-        methodProperties.put("AreaRef", "00000000-0000-0000-0000-000000000000");
-        methodProperties.put("Ref", "00000000-0000-0000-0000-000000000000");
-        methodProperties.put("RegionRef", "00000000-0000-0000-0000-000000000000");
+    public void setup() {
+
+
+        HashMap<String, Object> jsonObject = new HashMap<>();
+        HashMap<String, Object> methodProperties = new HashMap<>();
+
         methodProperties.put("Page", "1");
         methodProperties.put("Warehouse", "1");
-        methodProperties.put("FindByString", "Київ");
+        methodProperties.put("FindByString", "Львів");
         methodProperties.put("Limit", "20");
 
+        jsonObject.put("apiKey", "def7e7bd2304d0aa8c0a788f596985b6");
+        jsonObject.put("modelName", "Address");
+        jsonObject.put("calledMethod", "getSettlements");
+        jsonObject.put("methodProperties", methodProperties);
 
         given()
-                .param("apiKey", API_KEY)
-                .param("modelName", "Address")
-                .param("calledMethod", "getSettlements")
-                .params(methodProperties)
+                .header("content-type", "application/json")
+                .body(jsonObject).log().body()
                 .when()
-                .get(BASE_URL)
-                .then()
-                .statusCode(200)
-                .body("success", equalTo(true)) //в мене чомусь він фейлиться і не знаходить, я вже всю документацію там прочитав, але щось не можу знайти рішення((
-                .body("data[0].MainDescription", equalTo("Київ"));
+                .get("https://api.novaposhta.ua/v2.0/json/")
+                .then().log().all();
     }
 }
